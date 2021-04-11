6f7bfffa66d4cded96d5ff84ff794d6f8462fdaf
im-minion <vaibhavminiyar@gmail.com>
2021-04-11 16:13:50 +0530

    added Synch

---
+++ src/multithreading/synchronzation/Synch.java
@@ -0,0 +1,59 @@
+package multithreading.synchronzation;
+
+// This program is not synchronized.
+
+public class Synch {
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Hello");
+        Caller ob2 = new Caller(target, "Synchronized");
+        Caller ob3 = new Caller(target, "World");
+
+        // wait for threads to end
+        try {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+
+class Callme {
+    synchronized void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.print("]");
+    }
+}
+
+class Caller implements Runnable {
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme targ, String s) {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void run() {
+        synchronized (target) {
+            target.call(msg);
+        }
+    }
+}
+
+/*
+Here, the call( ) method is not modified by synchronized.
+Instead, the synchronized statement is used inside Caller’s run( ) method.
+This causes the same correct output as the preceding example, because each thread waits for the prior one to finish before proceeding.
+*/
